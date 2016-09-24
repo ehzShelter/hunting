13ee46c99e57d3a5f5735a5068296f4af8175721
ehzShelter <emrulhasanzawad@outlook.com>
2016-09-24 14:47:44 +0600

    basic done

---
+++ standard.py
@@ -0,0 +1,25 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+
+    try:
+        bsObj = BeautifulSoup(html.read(),"lxml")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+
+    return title
+
+
+# calling getTitle(url) func
+theTitle = getTitle("http://www.aiub.edu")
+if theTitle == None:
+    print("Title could not found")
+else:
+    print(theTitle)
